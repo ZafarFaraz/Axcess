8495161d8dd33198376527cfd4809d1ec418b514
zafar <zfaraz@synchron.com>
2024-07-08 13:59:46 +1000

    ttsPage has spcific tiles per section

--- lib/components/tts/tts_section.dart
+++ lib/components/tts/tts_section.dart
@@ -1,16 +1,45 @@
 class Section {
   String title;
-  List<Map<String, String>> phrases;
+  List<Phrase> phrases;
 
   Section(this.title, this.phrases);
 
-  Map<String, dynamic> toJson() => {
-        'title': title,
-        'phrases': phrases,
-      };
+  factory Section.fromJson(Map<String, dynamic> json) {
+    var phrasesFromJson = json['phrases'] as List;
+    List<Phrase> phraseList =
+        phrasesFromJson.map((i) => Phrase.fromJson(i)).toList();
 
-  factory Section.fromJson(Map<String, dynamic> json) => Section(
-        json['title'],
-        List<Map<String, String>>.from(json['phrases']),
-      );
+    return Section(
+      json['title'],
+      phraseList,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'title': title,
+      'phrases': phrases.map((e) => e.toJson()).toList(),
+    };
+  }
+}
+
+class Phrase {
+  String key;
+  String label;
+
+  Phrase(this.key, this.label);
+
+  factory Phrase.fromJson(Map<String, dynamic> json) {
+    return Phrase(
+      json['key'],
+      json['label'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'key': key,
+      'label': label,
+    };
+  }
 }
--- lib/pages/tts/tts_page.dart
+++ lib/pages/tts/tts_page.dart
@@ -1,11 +1,11 @@
 import 'dart:convert';
 import 'dart:io';
-
-import 'package:axcess/pages/tts/tts_content.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:path_provider/path_provider.dart';
-
 import '../../components/tts/tts_section.dart';
+import '../../components/tts/tts_button.dart';
 
 class TTSPage extends StatefulWidget {
   const TTSPage({super.key});
@@ -15,33 +15,74 @@ class TTSPage extends StatefulWidget {
 }
 
 class _TTSPageState extends State<TTSPage> {
-  bool editMode = false;
   List<Section> _sections = [];
-  int _selectedSectionIndex = 0;
   String? _jsonFilePath;
+  int _selectedSectionIndex = 0;
+  bool editMode = false;
+  late FlutterTts flutterTts;
+  TextEditingController _phraseController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
+    flutterTts = FlutterTts();
     _initJsonFile();
   }
 
-  void toggleEditMode(bool value) {
+  Future<void> _initJsonFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    _jsonFilePath = '${directory.path}/tts.json';
+    final file = File(_jsonFilePath!);
+
+    if (!await file.exists()) {
+      String jsonString = await rootBundle.loadString('lib/assets/tts.json');
+      await file.writeAsString(jsonString);
+      _sections = (json.decode(jsonString) as List)
+          .map((item) => Section.fromJson(item))
+          .toList();
+    } else {
+      _loadSections();
+    }
+  }
+
+  Future<void> _loadSections() async {
+    final file = File(_jsonFilePath!);
+    final String response = await file.readAsString();
+    final List<dynamic> data = json.decode(response);
     setState(() {
-      editMode = value;
+      _sections = data.map((item) => Section.fromJson(item)).toList();
     });
+  }
+
+  Future<void> _updateJsonFile() async {
+    final file = File(_jsonFilePath!);
+    await file
+        .writeAsString(json.encode(_sections.map((e) => e.toJson()).toList()));
   }
 
   void _addSection() {
     setState(() {
-      String newSection = 'Section ${_sections.length + 1}';
-      _sections.add(Section(newSection, []));
+      String newSectionTitle = 'Section ${_sections.length + 1}';
+      _sections.add(Section(newSectionTitle, []));
     });
     _updateJsonFile();
   }
 
+  void _removeSection() {
+    if (_sections.isNotEmpty) {
+      setState(() {
+        _sections.removeAt(_selectedSectionIndex);
+        _selectedSectionIndex =
+            (_selectedSectionIndex > 0) ? _selectedSectionIndex - 1 : 0;
+      });
+      _updateJsonFile();
+    }
+  }
+
   void _editSection() {
-    final _sectionTitleController = TextEditingController();
+    final sectionTitleController = TextEditingController(
+      text: _sections[_selectedSectionIndex].title,
+    );
 
     showDialog(
         context: context,
@@ -51,7 +92,7 @@ class _TTSPageState extends State<TTSPage> {
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   TextField(
-                    controller: _sectionTitleController,
+                    controller: sectionTitleController,
                     decoration: InputDecoration(labelText: 'Section Title'),
                   )
                 ],
@@ -69,7 +110,7 @@ class _TTSPageState extends State<TTSPage> {
                         String oldSection =
                             _sections[_selectedSectionIndex].title;
                         _sections[_selectedSectionIndex].title =
-                            _sectionTitleController.text;
+                            sectionTitleController.text;
                         _updateJsonFile();
                       });
                       Navigator.of(context).pop();
@@ -79,10 +120,17 @@ class _TTSPageState extends State<TTSPage> {
             ));
   }
 
-  void _removeSection() {
+  void _addPhrase(int sectionIndex, String label) {
     setState(() {
-      _sections.removeAt(_selectedSectionIndex);
-      _selectedSectionIndex = 0;
+      String newKey = 'custom_${_sections[sectionIndex].phrases.length}';
+      _sections[sectionIndex].phrases.add(Phrase(newKey, label));
+    });
+    _updateJsonFile();
+  }
+
+  void _removePhrase(int sectionIndex, int phraseIndex) {
+    setState(() {
+      _sections[sectionIndex].phrases.removeAt(phraseIndex);
     });
     _updateJsonFile();
   }
@@ -93,123 +141,39 @@ class _TTSPageState extends State<TTSPage> {
     });
   }
 
-  Future<void> _initJsonFile() async {
-    final directory = await getApplicationDocumentsDirectory();
-    _jsonFilePath = '${directory.path}/tts_sections.json';
-    final file = File(_jsonFilePath!);
-    if (!await file.exists()) {
-      await file.writeAsString(json.encode([]));
-    } else {
-      _loadSections();
-    }
-  }
-
-  Future<void> _loadSections() async {
-    final file = File(_jsonFilePath!);
-    final String response = await file.readAsString();
-    final List<dynamic> data = json.decode(response);
+  void toggleEditMode(bool value) {
     setState(() {
-      _sections = data.map((item) => Section.fromJson(item)).toList();
+      editMode = value;
+      Navigator.of(context).pop();
+      print(_sections.length);
     });
   }
 
-  Future<void> _updateJsonFile() async {
-    try {
-      final file = File(_jsonFilePath!);
-      await file.writeAsString(
-          json.encode(_sections.map((e) => e.toJson()).toList()));
-    } catch (e) {
-      print("Failed to update JSON file: $e");
-    }
+  Future<void> _speak(String text) async {
+    await flutterTts.setLanguage("en-US");
+    await flutterTts.setPitch(1.0);
+    await flutterTts.speak(text);
   }
 
   @override
   Widget build(BuildContext context) {
-    // Ensure there are at least two sections
-    if (_sections.isEmpty) {
-      _sections = [
-        Section('Section 1', []),
-        Section('Section 2', []),
-      ];
-    }
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Text to Speech'),
-      ),
-      body: Row(
-        children: [
-          if (editMode)
-            Column(
-              children: [
-                SizedBox(height: 350),
-                FloatingActionButton(
-                  onPressed: _addSection,
-                  child: Icon(Icons.add),
-                ),
-                FloatingActionButton(
-                  onPressed: _editSection,
-                  child: Icon(Icons.edit),
-                ),
-                FloatingActionButton(
-                  onPressed: _removeSection,
-                  child: Icon(Icons.remove),
-                ),
-              ],
-            ),
-          NavigationRail(
-            selectedIndex: _selectedSectionIndex,
-            onDestinationSelected: _onSectionSelected,
-            labelType: NavigationRailLabelType.selected,
-            destinations: [
-              ..._sections.map((section) => NavigationRailDestination(
-                    icon: Icon(
-                      Icons.home,
-                      size: 50,
-                    ),
-                    selectedIcon: Icon(
-                      Icons.home_filled,
-                      size: 100,
-                    ),
-                    label: SizedBox(
-                      width: 200,
-                      height: 100,
-                      child: Text(
-                        section.title,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 30),
-                      ),
-                    ),
-                  )),
-            ],
-          ),
-          Expanded(
-            child: TTSContent(
-              editMode: editMode,
-              section: _sections[_selectedSectionIndex],
-              onTtsItemsChanged: (items) {
-                setState(() {
-                  _sections[_selectedSectionIndex].phrases = items;
-                });
-                _updateJsonFile();
-              },
-            ),
-          ),
-        ],
       ),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
           children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(color: Colors.green),
+            DrawerHeader(
+              decoration: const BoxDecoration(color: Colors.green),
               child: SizedBox(
                 child: Text('Admin'),
               ),
             ),
             Row(
               children: [
-                const Padding(padding: EdgeInsets.all(16)),
+                Padding(padding: EdgeInsets.all(16)),
                 const Text('Edit Mode'),
                 Switch(value: editMode, onChanged: toggleEditMode)
               ],
@@ -217,6 +181,166 @@ class _TTSPageState extends State<TTSPage> {
           ],
         ),
       ),
+      body: _sections.isEmpty
+          ? Center(child: CircularProgressIndicator())
+          : Row(
+              children: [
+                if (editMode)
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      FloatingActionButton(
+                        onPressed: _addSection,
+                        child: Icon(Icons.add),
+                        tooltip: 'Add Section',
+                      ),
+                      const SizedBox(height: 16),
+                      FloatingActionButton(
+                        onPressed: _editSection,
+                        child: Icon(Icons.edit),
+                        tooltip: 'Edit Section',
+                      ),
+                      const SizedBox(height: 16),
+                      if (_sections.length > 2)
+                        FloatingActionButton(
+                          onPressed: _removeSection,
+                          child: Icon(Icons.remove),
+                          tooltip: 'Remove Section',
+                        ),
+                    ],
+                  ),
+                NavigationRail(
+                  selectedIndex: _selectedSectionIndex,
+                  onDestinationSelected: _onSectionSelected,
+                  labelType: NavigationRailLabelType.selected,
+                  destinations: _sections
+                      .map((section) => NavigationRailDestination(
+                            icon: Icon(
+                              Icons.folder,
+                              size: 30,
+                            ),
+                            selectedIcon: Icon(
+                              Icons.folder_open,
+                              size: 100,
+                            ),
+                            label: SizedBox(
+                              width: 200,
+                              height: 100,
+                              child: Text(
+                                section.title,
+                                textAlign: TextAlign.center,
+                                style: TextStyle(fontSize: 30),
+                              ),
+                            ),
+                          ))
+                      .toList(),
+                ),
+                VerticalDivider(thickness: 1, width: 1),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          children: [
+                            TextField(
+                              controller: _phraseController,
+                              decoration: InputDecoration(
+                                labelText: "Enter custom phrase",
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(16.0),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 8),
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: ElevatedButton(
+                                    onPressed: () {
+                                      if (_phraseController.text.isNotEmpty) {
+                                        _speak(_phraseController.text);
+                                      }
+                                    },
+                                    child: Text("Speak"),
+                                  ),
+                                ),
+                                const SizedBox(width: 8),
+                                Expanded(
+                                  child: ElevatedButton(
+                                    onPressed: () {
+                                      if (_phraseController.text.isNotEmpty) {
+                                        _addPhrase(_selectedSectionIndex,
+                                            _phraseController.text);
+                                        _phraseController.clear();
+                                      }
+                                    },
+                                    child: Text("Add"),
+                                  ),
+                                ),
+                                const SizedBox(width: 8),
+                                Expanded(
+                                  child: ElevatedButton(
+                                    onPressed: () {
+                                      _phraseController.clear();
+                                    },
+                                    child: Text("Clear"),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: GridView.builder(
+                          padding: const EdgeInsets.all(16.0),
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 3, // Number of items per row
+                            mainAxisSpacing: 16.0,
+                            crossAxisSpacing: 16.0,
+                            childAspectRatio: 2.5, // Aspect ratio for each item
+                          ),
+                          itemCount:
+                              _sections[_selectedSectionIndex].phrases.length,
+                          itemBuilder: (context, phraseIndex) {
+                            final phrase = _sections[_selectedSectionIndex]
+                                .phrases[phraseIndex];
+                            return Stack(
+                              children: [
+                                Positioned.fill(
+                                  child: TTSButton(
+                                    text: phrase.label,
+                                    onPressed: () {
+                                      _speak(phrase.label);
+                                    },
+                                    onLongPress:
+                                        () {}, // Required but not used here
+                                  ),
+                                ),
+                                if (editMode)
+                                  Positioned(
+                                    right: 8,
+                                    top: 8,
+                                    child: FloatingActionButton(
+                                      mini: true,
+                                      backgroundColor: Colors.red,
+                                      onPressed: () => _removePhrase(
+                                          _selectedSectionIndex, phraseIndex),
+                                      child: const Icon(Icons.close, size: 20),
+                                    ),
+                                  ),
+                              ],
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
     );
   }
 }
